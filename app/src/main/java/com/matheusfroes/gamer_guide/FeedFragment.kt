afa84df186dca414e8bfe84fc04fb3b58d9be2fb
Matheus Fróes Marques <matheusfroesmarques@gmail.com>
2017-09-19 16:42:55 -0300

    batch commit

---
+++ app/src/main/java/com/matheusfroes/gamer_guide/FeedFragment.kt
@@ -0,0 +1,17 @@
+package com.matheusfroes.gamer_guide
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by matheus_froes on 19/09/2017.
+ */
+class FeedFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+}
--- app/src/main/java/com/matheusfroes/gamer_guide/TelaPrincipalActivity.kt
+++ app/src/main/java/com/matheusfroes/gamer_guide/TelaPrincipalActivity.kt
@@ -1,79 +1,54 @@
 package com.matheusfroes.gamer_guide
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBarDrawerToggle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.Menu
 import android.view.MenuItem
-import android.view.View
+import kotlinx.android.synthetic.main.activity_tela_principal.*
+import kotlinx.android.synthetic.main.toolbar.*
 
-class TelaPrincipalActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class TelaPrincipalActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_principal)
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
-        val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.addDrawerListener(toggle)
-        toggle.syncState()
+        navigation.setOnNavigationItemSelectedListener(this)
 
-        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
-        navigationView.setNavigationItemSelectedListener(this)
+        navigation.selectedItemId = R.id.navigation_home
     }
 
-    override fun onBackPressed() {
-        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.tela_principal, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-
-    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        val id = item.itemId
-
-        when (id) {
-            R.id.nav_camera -> {
-                // Handle the camera action
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                mudarTela(0)
+                return true
             }
-            R.id.nav_gallery -> {
-
+            R.id.navigation_dashboard -> {
+                mudarTela(1)
+                return true
             }
-            R.id.nav_slideshow -> {
-
+            R.id.navigation_notifications -> {
+                mudarTela(2)
+                return true
             }
         }
-        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
-        drawer.closeDrawer(GravityCompat.START)
-        return true
+        return false
     }
+
+
+    private fun mudarTela(position: Int) {
+
+        val fragment = when (position) {
+            0 -> MeusJogosFragment()
+            1 -> FeedFragment()
+            2 -> ListasFragment()
+            else -> MeusJogosFragment()
+        }
+
+        supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
+    }
+
 }
