c6615bad030c0c737729ae0d4cff258b469f9d0b
Matheus Fróes Marques <matheusfroesmarques@gmail.com>
2017-10-05 11:08:03 -0300

    Seekbar fragment Meu Progresso

--- app/src/main/java/com/matheusfroes/gamerguide/fragments/MeuProgressoFragment.kt
+++ app/src/main/java/com/matheusfroes/gamerguide/fragments/MeuProgressoFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.matheusfroes.gamerguide.R
+import kotlinx.android.synthetic.main.fragment_meu_progresso.view.*
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar
 
 /**
  * Created by matheus_froes on 26/09/2017.
@@ -13,6 +15,20 @@ import com.matheusfroes.gamerguide.R
 class MeuProgressoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_meu_progresso, container, false)
+        val view = inflater.inflate(R.layout.fragment_meu_progresso, container, false)
+
+        view.sbProgresso.setOnProgressChangeListener(object : DiscreteSeekBar.OnProgressChangeListener {
+            override fun onProgressChanged(seekBar: DiscreteSeekBar?, value: Int, fromUser: Boolean) {
+                view.tvPorcentagemProgresso.text = "$value%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: DiscreteSeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: DiscreteSeekBar?) {}
+
+        })
+
+
+        return view
     }
 }
